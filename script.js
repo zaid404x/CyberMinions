35db155e3dc84be723a117674c0e889dccb288ec
zaid404 <pathanzaid973@gmail.com>
2026-06-23 16:24:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const button = document.getElementById("exploreBtn");
+let threatCount = 0;
+let alertCount = 0;
 
-button.addEventListener("click", () => {
-    alert("Welcome to CyberMinions 🚀");
-});
+const threatElement = document.getElementById("threats");
+const alertElement = document.getElementById("alerts");
+
+const threatInterval = setInterval(() => {
+    threatCount++;
+    threatElement.innerText = threatCount;
+
+    if(threatCount >= 245){
+        clearInterval(threatInterval);
+    }
+}, 15);
+
+const alertInterval = setInterval(() => {
+    alertCount++;
+    alertElement.innerText = alertCount;
+
+    if(alertCount >= 12){
+        clearInterval(alertInterval);
+    }
+}, 100);
